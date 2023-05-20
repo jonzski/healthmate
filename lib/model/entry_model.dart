b514a0ac6a8e7816b6565eff8bba3b8390a2876d
MarshmallowMann <jcdespi@up.edu.ph>
2023-05-20 13:21:38 +0800

    feat: added a model for user entries.

---
+++ lib/model/entry_model.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+class DailyEntry {
+  String uid;
+  String entryId;
+  Map<String, bool> symptoms;
+  bool closeContact;
+  DateTime entryDate;
+  late bool canGenerateQR;
+
+  DailyEntry({
+    required this.uid,
+    required this.entryId,
+    required this.symptoms,
+    required this.closeContact,
+    required this.entryDate,
+  }) {
+    this.canGenerateQR =
+        !(this.closeContact || this.symptoms.containsValue(true));
+  }
+
+  factory DailyEntry.fromJson(Map<String, dynamic> json) {
+    return DailyEntry(
+      uid: json['uid'],
+      entryId: json['entryId'],
+      symptoms: json['symptoms'],
+      closeContact: json['closeContact'],
+      entryDate: json['entryDate'],
+    );
+  }
+
+  static List<DailyEntry> fromJsonArray(String jsonData) {
+    final Iterable<dynamic> data = jsonDecode(jsonData);
+    return data.map<DailyEntry>((dynamic d) => DailyEntry.fromJson(d)).toList();
+  }
+
+  Map<String, dynamic> toJson(DailyEntry entry) {
+    return {
+      'uid': entry.uid,
+      'entryId': entry.entryId,
+      'symptoms': entry.symptoms,
+      'closeContact': entry.closeContact,
+      'entryDate': entry.entryDate,
+    };
+  }
+}
