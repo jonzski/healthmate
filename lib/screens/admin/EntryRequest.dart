64a4619c7b61351f2302ef63d08a4c0f6d48e8ff
jonzski <jhperez4@up.edu.ph>
2023-05-27 18:41:36 +0800

    Add: Pageview stream for edit request

--- lib/screens/admin/EntryRequest.dart
+++ lib/screens/admin/EntryRequest.dart
@@ -1,44 +1,60 @@
 import 'package:flutter/material.dart';
-import '../../provider/user_provider.dart';
+import '../../model/entry_model.dart';
+import '../../provider/entry_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class EntryRequest extends StatefulWidget {
-  const EntryRequest({Key? key}) : super(key: key);
+class StudentMonitoring extends StatefulWidget {
+  const StudentMonitoring({Key? key}) : super(key: key);
 
   @override
-  State<EntryRequest> createState() => _EntryRequestState();
+  State<StudentMonitoring> createState() => _StudentMonitoringState();
 }
 
-class _EntryRequestState extends State<EntryRequest> {
+class _StudentMonitoringState extends State<StudentMonitoring> {
   @override
   Widget build(BuildContext context) {
-    Stream<QuerySnapshot> allstudents =
-        context.watch<UserProvider>().allStudents;
+    Stream<QuerySnapshot> editRequests =
+        context.watch<EntryProvider>().allRequestedEditEntries;
 
-    return StreamBuilder(
-      stream: allstudents,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Center(
-            child: Text("Error encountered! ${snapshot.error}"),
+    return Container(
+      color: const Color(0xFF090c12),
+      child: StreamBuilder(
+        stream: editRequests,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Error encountered! ${snapshot.error}"),
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.data!.docs.isEmpty) {
+            return const Center(
+              child: Text("No students found."),
+            );
+          }
+
+          return Padding(
+            padding: const EdgeInsets.all(
+                16.0), // Adjust the padding value as needed
+            child: ListView.builder(
+              itemCount: snapshot.data?.docs.length,
+              itemBuilder: ((context, index) {
+                DailyEntry entry = DailyEntry.fromJson(
+                    snapshot.data?.docs[index].data() as Map<String, dynamic>);
+                return Card(
+                  color: const Color(0xFF222429),
+                  child: ListTile(
+                    title: Text(entry.uid),
+                  ),
+                );
+              }),
+            ),
           );
-        } else if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (snapshot.data!.docs.isEmpty) {
-          return const Center(
-            child: Text("No students found."),
-          );
-        }
-        return ListView.builder(
-          itemCount: snapshot.data?.docs.length,
-          itemBuilder: ((context, index) {
-            print(index);
-          }),
-        );
-      },
+        },
+      ),
     );
   }
 }
