e7ea7919018fa2678961becd278700a39d5d8650
jrppena <jppena@up.edu.ph>
2023-05-27 15:58:31 +0800

    feat: added api for adding and updating logs

--- lib/model/log_model.dart
+++ lib/model/log_model.dart
@@ -1,27 +1,33 @@
 import 'dart:convert';
 
 class MonitorLog {
-  final String? status;
-  final String studentNum;
+  String? status;
+  String? studentNum;
   final String userId;
   final DateTime date;
-  final String location;
+  String? location;
+  final String logId;
+  String? studentId;
 
   MonitorLog({
     this.status,
-    required this.studentNum,
+    this.studentNum,
     required this.userId,
     required this.date,
-    required this.location,
+    this.location,
+    required this.logId,
+    this.studentId,
   });
 
   factory MonitorLog.fromJson(Map<String, dynamic> json) {
     return MonitorLog(
-        status: json['status'],
-        studentNum: json['studentNum'],
-        userId: json['userId'],
-        date: json['date'].toDate(),
-        location: json['location']);
+      status: json['status'],
+      studentNum: json['studentNum'],
+      userId: json['userId'],
+      date: json['date'].toDate(),
+      location: json['location'],
+      logId: json['logId'],
+    );
   }
 
   static List<MonitorLog> fromJsonArray(String jsonData) {
