7c295af9cec441837fbf0c35223c71a41e46d023
MarshmallowMann <jcdespi@up.edu.ph>
2023-05-27 15:11:41 +0800

    feat: added way to view all logs and view logs by date.

--- lib/api/firebase_log_api.dart
+++ lib/api/firebase_log_api.dart
@@ -5,9 +5,27 @@ import '../model/user_model.dart';
 class FirebaseLogAPI {
   static final FirebaseFirestore db = FirebaseFirestore.instance;
 
-  Stream<QuerySnapshot<Object?>> fetchAllLogs() {
+  Stream<QuerySnapshot<Object?>> fetchAllLogs(String userId) {
     try {
-      return db.collection("log").orderBy('date', descending: true).snapshots();
+      return db
+          .collection("log")
+          .where('userId', isEqualTo: userId)
+          .orderBy('date', descending: true)
+          .snapshots();
+    } on FirebaseException catch (e) {
+      throw e;
+    }
+  }
+
+  Stream<QuerySnapshot<Object?>> fetchAllLogsByDate(
+      DateTime date, String userId) {
+    try {
+      return db
+          .collection("log")
+          .where('userId', isEqualTo: userId)
+          .where('date', isEqualTo: date)
+          .orderBy('date', descending: true)
+          .snapshots();
     } on FirebaseException catch (e) {
       throw e;
     }
