215508048d3e6e4b3f09d579e79423609ff15fdc
jonzski <jhperez4@up.edu.ph>
2023-05-31 11:03:52 +0800

    Feat: Correctly display user profile

--- lib/screens/user/Profile.dart
+++ lib/screens/user/Profile.dart
@@ -1,4 +1,5 @@
 import 'package:cmsc_23_project/provider/auth_provider.dart';
+import '../../provider/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -7,70 +8,93 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: const Color(0xFF090c12),
-        child: Padding(
-            padding: const EdgeInsets.all(30),
-            child: Center(
-              child: ListView(shrinkWrap: true, children: <Widget>[
-                const Center(
-                    child: Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Text(
-                          "Profile",
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.bold),
-                        ))),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 5),
-                  child: Text(
-                    "Name: Goofy ahh lil jits",
-                    style: TextStyle(fontSize: 18),
+    String currentUserUid = context.read<AuthProvider>().currentUser.uid;
+
+    return FutureBuilder<Map<String, dynamic>?>(
+      future: context.read<UserProvider>().viewSpecificStudent(currentUserUid),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("Error encountered! ${snapshot.error}"),
+          );
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        Map<String, dynamic>? user = snapshot.data;
+
+        String name = user?['name'];
+        String studentNum = user?['studentNum'];
+        String course = user?['course'];
+        String college = user?['college'];
+
+        return Container(
+          color: const Color(0xFF090c12),
+          child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: Center(
+                child: ListView(shrinkWrap: true, children: <Widget>[
+                  const Center(
+                      child: Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Text(
+                            "Profile",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ))),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      "Name: $name",
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 5),
-                  child: Text(
-                    "Student Number: 2021-72769",
-                    style: TextStyle(fontSize: 18),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      "Student Number: $studentNum",
+                      style: const TextStyle(fontSize: 18),
+                    ),
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 5),
-                  child: Text(
-                    "Course: BS Computer Science",
-                    style: TextStyle(fontSize: 18),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      "College: $college",
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 5),
-                  child: Text(
-                    "College: College of Arts and Sciences",
-                    style: TextStyle(fontSize: 18),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      "Course: $course",
+                      style: const TextStyle(fontSize: 18),
+                    ),
                   ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    context.read<AuthProvider>().signOut();
-                    Navigator.pushReplacementNamed(context, '/login');
-                  },
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.transparent),
-                    overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  ),
-                  child: const Text(
-                    'Logout',
-                    style: TextStyle(
-                        fontFamily: 'SF-UI-Display',
-                        fontWeight: FontWeight.w700,
-                        fontSize: 15),
-                  ),
-                )
-              ]),
-            )),
-      ),
+                  TextButton(
+                    onPressed: () {
+                      context.read<AuthProvider>().signOut();
+                      Navigator.pushReplacementNamed(context, '/login');
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.transparent),
+                      overlayColor:
+                          MaterialStateProperty.all(Colors.transparent),
+                    ),
+                    child: const Text(
+                      'Logout',
+                      style: TextStyle(
+                          fontFamily: 'SF-UI-Display',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 15),
+                    ),
+                  )
+                ]),
+              )),
+        );
+      },
     );
   }
 }
