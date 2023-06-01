a0c019263679f69ef462e2cebf880db9ed3f7ad5
Diana Compahinay <dpcompahinay@up.edu.ph>
2023-06-01 23:09:18 +0800

    fix: display all entries of current user.

--- lib/screens/user/Profile.dart
+++ lib/screens/user/Profile.dart
@@ -31,69 +31,94 @@ class Profile extends StatelessWidget {
         String college = user?['college'];
 
         return Container(
-          color: const Color(0xFF090c12),
-          child: Padding(
-              padding: const EdgeInsets.all(30),
-              child: Center(
-                child: ListView(shrinkWrap: true, children: <Widget>[
-                  const Center(
-                      child: Padding(
-                          padding: EdgeInsets.all(15),
-                          child: Text(
-                            "Profile",
-                            style: TextStyle(
-                                fontSize: 30, fontWeight: FontWeight.bold),
-                          ))),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Text(
-                      "Name: $name",
-                      style: TextStyle(fontSize: 18),
-                    ),
+            width: 500,
+            color: const Color(0xFF090c12),
+            child: Center(
+                child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(15),
+                  child: const Center(
+                      child: Text(
+                    "MyProfile",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  )),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                      top: 20, bottom: 0, left: 40, right: 40),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Text(
-                      "Student Number: $studentNum",
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Text(
-                      "College: $college",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Text(
-                      "Course: $course",
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      context.read<AuthProvider>().signOut();
-                      Navigator.pushReplacementNamed(context, '/login');
-                    },
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.transparent),
-                      overlayColor:
-                          MaterialStateProperty.all(Colors.transparent),
-                    ),
-                    child: const Text(
-                      'Logout',
-                      style: TextStyle(
-                          fontFamily: 'SF-UI-Display',
-                          fontWeight: FontWeight.w700,
-                          fontSize: 15),
-                    ),
-                  )
-                ]),
-              )),
-        );
+                  child: Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Center(
+                        child: ListView(shrinkWrap: true, children: <Widget>[
+                          const Center(
+                              child: Padding(
+                                  padding: EdgeInsets.all(15),
+                                  child: Text(
+                                    "Profile",
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold),
+                                  ))),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Text(
+                              "Name: $name",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Text(
+                              "Student Number: $studentNum",
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Text(
+                              "College: $college",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Text(
+                              "Course: $course",
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              context.read<AuthProvider>().signOut();
+                              Navigator.pushReplacementNamed(context, '/login');
+                            },
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.transparent),
+                              overlayColor:
+                                  MaterialStateProperty.all(Colors.transparent),
+                            ),
+                            child: const Text(
+                              'Logout',
+                              style: TextStyle(
+                                  fontFamily: 'SF-UI-Display',
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 15),
+                            ),
+                          )
+                        ]),
+                      )),
+                ),
+              ],
+            )));
       },
     );
   }
--- lib/screens/user/UserEntries.dart
+++ lib/screens/user/UserEntries.dart
@@ -18,137 +18,131 @@ class _UserEntriesState extends State<UserEntries> {
   Widget build(BuildContext context) {
     Stream<QuerySnapshot> entriesStream =
         context.watch<EntryProvider>().allEntries;
-
     return Container(
       color: const Color(0xFF090c12),
       child: StreamBuilder(
-        stream: entriesStream,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
+          stream: entriesStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("Error encountered! ${snapshot.error}"),
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.data!.docs.isEmpty) {
+              return const Center(
+                child: Text("No entries found."),
+              );
+            }
             return Center(
-              child: Text("Error encountered! ${snapshot.error}"),
-            );
-          } else if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.data!.docs.isEmpty) {
-            return const Center(
-              child: Text("No entries found."),
-            );
-          }
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              const SizedBox(height: 20.0),
-              const Padding(
-                padding: EdgeInsets.only(bottom: 10),
-                child: Text(
-                  'List of Entries',
-                  textAlign: TextAlign.center,
+                child: ListView(children: [
+              Container(
+                margin: const EdgeInsets.all(15),
+                child: const Center(
+                    child: Text(
+                  "List of Entries",
                   style: TextStyle(fontFamily: 'SF-UI-Display', fontSize: 25),
-                ),
+                )),
               ),
-              Expanded(
-                  child: Padding(
-                padding:
-                    EdgeInsets.all(16.0), // Adjust the padding value as needed
-                child: ListView.builder(
-                  itemCount: snapshot.data?.docs.length,
-                  itemBuilder: ((context, index) {
-                    DailyEntry entry = DailyEntry.fromJson(
-                        snapshot.data?.docs[index].data()
-                            as Map<String, dynamic>,
-                        'fetch');
-                    String date =
-                        DateFormat('dd MMMM yyyy').format(entry.entryDate);
-                    String currentUserUid =
-                        context.read<AuthProvider>().currentUser.uid;
-
-                    if (entry.uid == currentUserUid) {
-                      return Container(
-                          margin: const EdgeInsets.only(
-                              right: 40, left: 40, top: 10, bottom: 10),
-                          padding: const EdgeInsets.all(5),
-                          // height: 90,
-                          decoration: const BoxDecoration(
-                              color: Color(0xFF222429),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10))),
-                          child: Table(
-                            columnWidths: const {
-                              0: FixedColumnWidth(130),
-                              1: FixedColumnWidth(100),
-                              2: FixedColumnWidth(190),
-                            },
-                            children: [
-                              TableRow(
-                                children: [
-                                  Container(
-                                    margin: const EdgeInsets.all(5),
-                                    padding: const EdgeInsets.all(8),
-                                    decoration: const BoxDecoration(
-                                        color: Colors.indigo,
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(5))),
-                                    child: const Center(child: Text('Date')),
-                                  ),
-                                  Container(
-                                    margin: const EdgeInsets.all(5),
-                                    padding: const EdgeInsets.all(8),
-                                    decoration: const BoxDecoration(
-                                        color: Colors.indigo,
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(5))),
-                                    child:
-                                        const Center(child: Text('In Contact')),
-                                  ),
-                                  Container(
-                                    margin: const EdgeInsets.all(5),
-                                    padding: const EdgeInsets.all(8),
-                                    decoration: const BoxDecoration(
-                                        color: Colors.indigo,
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(5))),
-                                    child:
-                                        const Center(child: Text('Symptoms')),
-                                  ),
-                                ],
-                              ),
-                              TableRow(
-                                children: [
-                                  Container(
-                                      margin: const EdgeInsets.only(
-                                          top: 10,
-                                          bottom: 10,
-                                          left: 5,
-                                          right: 5),
-                                      child: Center(
-                                        child: Text(date),
-                                      )),
-                                  Container(
-                                    margin: const EdgeInsets.only(
-                                        top: 10, bottom: 10, left: 5, right: 5),
-                                    child: Center(child: inContact(entry)),
-                                  ),
-                                  Container(
-                                    margin: const EdgeInsets.only(
-                                        top: 10, bottom: 10, left: 5, right: 5),
-                                    child: Center(child: symptoms(entry)),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ));
-                    }
-                  }),
-                ),
-              )),
-            ],
-          );
-        },
-      ),
+              SizedBox(
+                width: 600,
+                child: dailyEntryStatus(snapshot),
+              )
+            ]));
+          }),
     );
+  }
+
+  dynamic dailyEntryStatus(AsyncSnapshot snapshot) {
+    List<Container> entryStatus = [];
+    for (int index = 0; index < snapshot.data?.docs.length; index++) {
+      DailyEntry entry = DailyEntry.fromJson(
+          snapshot.data?.docs[index].data() as Map<String, dynamic>, 'fetch');
+      String date = DateFormat('dd MMMM yyyy').format(entry.entryDate);
+      if (entry.uid == context.read<AuthProvider>().currentUser.uid) {
+        entryStatus.add(Container(
+            margin:
+                const EdgeInsets.only(right: 40, left: 40, top: 10, bottom: 10),
+            padding: const EdgeInsets.all(5),
+            // height: 90,
+            decoration: const BoxDecoration(
+                color: Color(0xFF222429),
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Table(
+              columnWidths: const {
+                0: FixedColumnWidth(130),
+                1: FixedColumnWidth(100),
+                2: FixedColumnWidth(190),
+              },
+              children: [
+                TableRow(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(5),
+                      padding: const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                          color: Colors.indigo,
+                          borderRadius: BorderRadius.all(Radius.circular(5))),
+                      child: const Center(child: Text('Date')),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(5),
+                      padding: const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                          color: Colors.indigo,
+                          borderRadius: BorderRadius.all(Radius.circular(5))),
+                      child: const Center(child: Text('In Contact')),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(5),
+                      padding: const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                          color: Colors.indigo,
+                          borderRadius: BorderRadius.all(Radius.circular(5))),
+                      child: const Center(child: Text('Symptoms')),
+                    ),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    Container(
+                        margin: const EdgeInsets.only(
+                            top: 10, bottom: 10, left: 5, right: 5),
+                        child: Center(
+                          child: Text(date),
+                        )),
+                    Container(
+                      margin: const EdgeInsets.only(
+                          top: 10, bottom: 10, left: 5, right: 5),
+                      child: Center(child: inContact(entry)),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(
+                          top: 10, bottom: 10, left: 5, right: 5),
+                      child: Center(child: symptoms(entry)),
+                    ),
+                  ],
+                ),
+              ],
+            )));
+      }
+    }
+    if (entryStatus.isEmpty) {
+      return Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+            Text(
+              "No Entries Yet!\n",
+              style: TextStyle(fontSize: 16),
+            ),
+          ]));
+    }
+
+    return Column(children: entryStatus);
   }
 
   Text inContact(DailyEntry entry) {
