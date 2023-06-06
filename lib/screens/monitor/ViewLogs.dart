80958b4243c29a35b0c1c68f8875c2872298c5e5
jonzski <jhperez4@up.edu.ph>
2023-06-07 02:36:02 +0800

    Feat: Add functionalities

--- lib/screens/monitor/ViewLogs.dart
+++ lib/screens/monitor/ViewLogs.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../../model/log_model.dart';
+import '../../model/user_model.dart';
 import '../../provider/log_provider.dart';
 import '../../provider/auth_provider.dart';
 import 'package:provider/provider.dart';
@@ -16,6 +17,16 @@ class ViewLogs extends StatefulWidget {
 }
 
 class _ViewLogsState extends State<ViewLogs> {
+  final TextEditingController _locationController = TextEditingController();
+
+  static final List<String> _statuses = [
+    "Cleared",
+    "Under Monitoring",
+    "Quarantine"
+  ];
+
+  String _statusValue = _statuses.first;
+
   @override
   Widget build(BuildContext context) {
     String currentUserUid = context.read<AuthProvider>().currentUser.uid;
@@ -79,7 +90,96 @@ class _ViewLogsState extends State<ViewLogs> {
                       color: const Color(0xFF222429),
                       child: ListTile(
                         title: Text(name),
-                        subtitle: Text('Date: $date'),
+                        trailing: Container(
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF526bf2),
+                          ),
+                          child: IconButton(
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return StatefulBuilder(
+                                    builder: (BuildContext context,
+                                        StateSetter setState) {
+                                      return AlertDialog(
+                                        backgroundColor:
+                                            const Color(0xFF222429),
+                                        title: const Text('Edit logs'),
+                                        content: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: <Widget>[
+                                            TextField(
+                                              controller: _locationController,
+                                              decoration: const InputDecoration(
+                                                hintText: 'Location',
+                                              ),
+                                            ),
+                                            DropdownButton<String>(
+                                              isExpanded: true,
+                                              value: _statusValue,
+                                              onChanged: (String? value) {
+                                                setState(() {
+                                                  _statusValue = value!;
+                                                });
+                                              },
+                                              items: _statuses.map<
+                                                  DropdownMenuItem<String>>(
+                                                (String value) {
+                                                  return DropdownMenuItem<
+                                                      String>(
+                                                    value: value,
+                                                    child: Text(value),
+                                                  );
+                                                },
+                                              ).toList(),
+                                            ),
+                                          ],
+                                        ),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            child: const Text('Edit'),
+                                            onPressed: () {
+                                              String location =
+                                                  _locationController.text;
+
+                                              context
+                                                  .read<LogProvider>()
+                                                  .updateLocation(
+                                                      logs.logId, location);
+
+                                              context
+                                                  .read<LogProvider>()
+                                                  .updateStatus(
+                                                      logs.logId,
+                                                      logs.studentId,
+                                                      _statusValue);
+
+                                              // Close the dialog
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                          TextButton(
+                                            child: const Text('Cancel'),
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                              );
+                            },
+                            icon: const Icon(Icons.edit),
+                            color: Colors.white,
+                            hoverColor: Colors.transparent,
+                            splashColor: Colors.transparent,
+                          ),
+                        ),
+                        subtitle: Text('$date | ${logs.location}'),
                       ),
                     );
                   },
