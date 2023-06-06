2c72620fb0bc1bd23342212dd1e6557a97ac9053
Diana Compahinay <dpcompahinay@up.edu.ph>
2023-06-07 07:48:27 +0800

    feat: logs can be searched by student name.

--- lib/screens/monitor/ViewLogs.dart
+++ lib/screens/monitor/ViewLogs.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../../model/log_model.dart';
+import '../../model/user_model.dart';
 import '../../provider/log_provider.dart';
 import '../../provider/auth_provider.dart';
 import 'package:provider/provider.dart';
@@ -16,12 +17,36 @@ class ViewLogs extends StatefulWidget {
 }
 
 class _ViewLogsState extends State<ViewLogs> {
+  final TextEditingController _searchController = TextEditingController();
+
+  List<MonitorLog> logs = [];
+  List<MonitorLog> searchedLogs = [];
+
+  bool startSearch = false;
+
+  void search(String searchText) {
+    setState(() {
+      searchedLogs.clear();
+      for (int index = 0; index < logs.length; index++) {
+        Future<Map<String, dynamic>?> student = context
+            .read<UserProvider>()
+            .viewSpecificStudent(logs[index].studentId);
+
+        student.then((data) {
+          if (data != null &&
+              data['name'].toLowerCase().startsWith(searchText.toLowerCase())) {
+            searchedLogs.add(logs[index]);
+          }
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     String currentUserUid = context.read<AuthProvider>().currentUser.uid;
 
     context.watch<LogProvider>().fetchAllLogs(currentUserUid);
-
     Stream<QuerySnapshot> allStudents = context.watch<LogProvider>().allLogs;
 
     return Container(
@@ -43,49 +68,83 @@ class _ViewLogsState extends State<ViewLogs> {
             );
           }
 
-          return Padding(
-            padding: const EdgeInsets.all(
-                16.0), // Adjust the padding value as needed
-            child: ListView.builder(
-              itemCount: snapshot.data?.docs.length,
-              itemBuilder: ((context, index) {
-                MonitorLog logs = MonitorLog.fromJson(
-                  snapshot.data?.docs[index].data() as Map<String, dynamic>,
-                );
+          List<MonitorLog> temp = [];
+          for (int index = 0; index < snapshot.data!.docs.length; index++) {
+            MonitorLog log = MonitorLog.fromJson(
+                snapshot.data?.docs[index].data() as Map<String, dynamic>);
+            temp.add(log);
+          }
+          logs = temp;
 
-                String date = DateFormat('dd MMMM yyyy').format(logs.date);
+          return Column(
+            children: [
+              Container(
+                  margin:
+                      const EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
+                  padding: const EdgeInsets.only(left: 15, right: 15),
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF526bf2),
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                  ),
+                  child: Center(
+                      child: Container(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: TextField(
+                              onChanged: (value) {
+                                search(value);
+                                startSearch = true;
+                              },
+                              controller: _searchController,
+                              style: const TextStyle(fontSize: 16),
+                              decoration: const InputDecoration(
+                                labelText: 'Search logs by student name',
+                                labelStyle: TextStyle(height: 1),
+                              ))))),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(
+                    16.0), // Adjust the padding value as needed
+                child: ListView.builder(
+                  itemCount: startSearch ? searchedLogs.length : logs.length,
+                  itemBuilder: ((context, index) {
+                    String date = DateFormat('dd MMMM yyyy').format(startSearch
+                        ? searchedLogs[index].date
+                        : logs[index].date);
 
-                return FutureBuilder<Map<String, dynamic>?>(
-                  future: context
-                      .read<UserProvider>()
-                      .viewSpecificStudent(logs.studentId),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return Center(
-                        child: Text("Error encountered! ${snapshot.error}"),
-                      );
-                    } else if (snapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
+                    return FutureBuilder<Map<String, dynamic>?>(
+                      future: context.read<UserProvider>().viewSpecificStudent(
+                          startSearch
+                              ? searchedLogs[index].studentId
+                              : logs[index].studentId),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          return Center(
+                            child: Text("Error encountered! ${snapshot.error}"),
+                          );
+                        } else if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
 
-                    Map<String, dynamic>? user = snapshot.data;
+                        Map<String, dynamic>? user = snapshot.data;
 
-                    String name = user?['name'];
+                        String name = user?['name'];
 
-                    return Card(
-                      color: const Color(0xFF222429),
-                      child: ListTile(
-                        title: Text(name),
-                        subtitle: Text('Date: $date'),
-                      ),
+                        return Card(
+                          color: const Color(0xFF222429),
+                          child: ListTile(
+                            title: Text(name),
+                            subtitle: Text('Date: $date'),
+                          ),
+                        );
+                      },
                     );
-                  },
-                );
-              }),
-            ),
+                  }),
+                ),
+              ))
+            ],
           );
         },
       ),
