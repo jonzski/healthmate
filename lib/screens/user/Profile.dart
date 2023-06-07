d484015e3645f19eb6ceffd1f6fb86543ccaf039
jonzski <jhperez4@up.edu.ph>
2023-06-07 14:20:04 +0800

    Fix: Change logout route

--- lib/screens/user/Profile.dart
+++ lib/screens/user/Profile.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'package:cmsc_23_project/model/user_model.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
